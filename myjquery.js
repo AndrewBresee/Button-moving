24e873108c0f99c30a9cc6ecb1b150696f62301f
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-01-03 19:59:02 -0800

    The button now moves randomly around the screen.
    Got the button to move to a random spot on the screen when clicked.
    I plan on starting the timer now, and fixing the layout a bit more.

--- myjquery.js
+++ myjquery.js
@@ -2,11 +2,26 @@
 
 
   	$("#the-button").on('click', function(){
+
+  		$(this).html("Click!");
+
   		var num = parseInt($.trim($("#click-number").html()));
   		$("#click-number").html(++num)
+
+
+	  	var h = $(window).height() - 50;
+	    var w = $(window).width() - 50;
+	    
+	    var nh = Math.floor(Math.random() * h);
+	    var nw = Math.floor(Math.random() * w);
+
+	    $("#the-button").animate({"top": nh, "left": nw });
+
+  
   	})
 
 
 
 });
- 
+//When the page loads, the button starts off as "start"
+//When "start" is clicked, it becomes "Click!"
